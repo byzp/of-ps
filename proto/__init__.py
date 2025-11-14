355dc68878840758b7001bc1650694c60643fdb5
byzp <1394026378@qq.com>
2025-11-14 18:22:55 +0800

    拆分proto

---
+++ proto/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import types
+from . import net_pb2
+
+_overfield = types.ModuleType('proto.OverField_pb2')
+_overfield.__package__ = 'proto'
+_overfield.__file__ = __file__
+
+sys.modules['proto.OverField_pb2'] = net_pb2
