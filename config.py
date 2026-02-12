e8b701922b7075f09b3963f867842d3ecd5c037f
byzp <1394026378@qq.com>
2026-02-12 22:22:54 +0800

    更新proto

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     # HTTP Server
     HTTP_HOST = "0.0.0.0"
     HTTP_PORT = 21000
-    RES_VERSION = "2025-12-04-20-38-15_2026-01-29-11-28-33"
+    RES_VERSION = "2026-01-27-16-45-02_2026-02-12-15-03-24"
     RES_URL = "http://cdn-of.inutan.com/Resources;https://cdn-of.inutan.com/Resources;http://127.0.0.1:21000/Resources"
 
     GAME_SERVER_GADGET_IP = "127.0.0.1"  # access
