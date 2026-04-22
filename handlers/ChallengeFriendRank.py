b1263b1c80169f2e69a0345e5bc400df387f8792
byzp <rocpiglet@gmail.com>
2026-04-22 21:24:09 +0800

    地图挑战排行

--- handlers/ChallengeFriendRank.py
+++ handlers/ChallengeFriendRank.py
@@ -1,14 +1,17 @@
 from network.packet_handler import PacketHandler, packet_handler
 from network.msg_id import MsgId
-import logging
 
-from proto.net_pb2 import ChallengeFriendRankReq, ChallengeFriendRankRsp, StatusCode
+from proto.net_pb2 import (
+    ChallengeFriendRankReq,
+    ChallengeFriendRankRsp,
+    StatusCode,
+    ChallengeData,
+)
 
-logger = logging.getLogger(__name__)
+import utils.db as db
+from utils.pb_create import make_PlayerBriefInfo
 
 
-# 挑战好友排行信息列表
-# 1301 1302
 @packet_handler(MsgId.ChallengeFriendRankReq)
 class Handler(PacketHandler):
     def handle(self, session, data: bytes, packet_id: int):
@@ -18,114 +21,30 @@ class Handler(PacketHandler):
         rsp = ChallengeFriendRankRsp()
         rsp.status = StatusCode.StatusCode_OK
 
-        # Hardcoded test data
-        # Set status
-        rsp.status = TEST_DATA["status"]
+        c = rsp.self_challenge
+        c.player_id = session.player_id
+        c_data = ChallengeData()
+        for i in db.get_challenge(session.player_id, session.scene_id):
+            c_data.ParseFromString(i)
+            tmp = c.challenge_infos.add()
+            tmp.challenge_id = c_data.challenge_id
+            tmp.use_time = c_data.use_time
 
-        # Add rank info
-        for rank_data in TEST_DATA["rank_info"]:
-            rank_info = rsp.rank_info.add()
-
-            # Player info
-            player_info = rank_info.player_info
-            player_data = rank_data["player_info"]
-            for key, value in player_data.items():
-                setattr(player_info, key, value)
-
-            # Challenge info
-            challenge = rank_info.challenge
-            challenge_data = rank_data["challenge"]
-            challenge.player_id = challenge_data["player_id"]
-
-            for challenge_info_data in challenge_data["challenge_infos"]:
-                challenge_info = challenge.challenge_infos.add()
-                challenge_info.challenge_id = challenge_info_data["challenge_id"]
-                challenge_info.use_time = challenge_info_data["use_time"]
-
-        # Self challenge
-        self_challenge = rsp.self_challenge
-        self_challenge_data = TEST_DATA["self_challenge"]
-        self_challenge.player_id = self_challenge_data["player_id"]
-
-        for challenge_info_data in self_challenge_data["challenge_infos"]:
-            challenge_info = self_challenge.challenge_infos.add()
-            challenge_info.challenge_id = challenge_info_data["challenge_id"]
-            challenge_info.use_time = challenge_info_data["use_time"]
+        for friend_info in db.get_friend_info(
+            session.player_id,
+            None,
+            "friend_id,friend_status",
+        ):
+            if friend_info[1] == 2:
+                info = rsp.rank_info.add()
+                make_PlayerBriefInfo(friend_info[0], info.player_info)
+                c = info.challenge
+                c.player_id = friend_info[0]
+                c_data = ChallengeData()
+                for i in db.get_challenge(friend_info[0], session.scene_id):
+                    c_data.ParseFromString(i)
+                    tmp = c.challenge_infos.add()
+                    tmp.challenge_id = c_data.challenge_id
+                    tmp.use_time = c_data.use_time
 
         session.send(MsgId.ChallengeFriendRankRsp, rsp, packet_id)
-
-
-# Hardcoded test data
-TEST_DATA = {
-    "status": 1,
-    "rank_info": [
-        {
-            "player_info": {
-                "player_id": 1111111,
-                "nick_name": "测试123",
-                "level": 50,
-                "head": 9345,
-                "last_login_time": 0,
-                "team_leader_badge": 5047,
-                "sex": 0,
-                "phone_background": 8031,
-                "is_online": False,
-                "sign": "测试123",
-                "guild_name": "",
-                "character_id": 202004,
-                "create_time": 1743502390,
-                "player_label": 2401700,
-                "garden_like_num": 379,
-                "account_type": 30114,
-                "birthday": "1992-09-14",
-                "hide_value": 0,
-                "avatar_frame": 7222,
-            },
-            "challenge": {
-                "player_id": 1111111,
-                "challenge_infos": [
-                    {"challenge_id": 400003, "use_time": 12521},
-                    {"challenge_id": 1052002, "use_time": 40222},
-                ],
-            },
-        },
-        {
-            "player_info": {
-                "player_id": 2921793,
-                "nick_name": "测试234",
-                "level": 45,
-                "head": 9345,
-                "last_login_time": 0,
-                "team_leader_badge": 5048,
-                "sex": 0,
-                "phone_background": 8032,
-                "is_online": True,
-                "sign": "测试234",
-                "guild_name": "",
-                "character_id": 202004,
-                "create_time": 1743502390,
-                "player_label": 2401700,
-                "garden_like_num": 379,
-                "account_type": 30114,
-                "birthday": "1992-09-14",
-                "hide_value": 0,
-                "avatar_frame": 7222,
-            },
-            "challenge": {
-                "player_id": 2222222,
-                "challenge_infos": [
-                    {"challenge_id": 400003, "use_time": 12521},
-                    {"challenge_id": 1052002, "use_time": 40222},
-                ],
-            },
-        },
-    ],
-    "self_challenge": {
-        "player_id": 1234567,
-        "challenge_infos": [
-            {"challenge_id": 203002, "use_time": 24003},
-            {"challenge_id": 1052001, "use_time": 23709},
-            {"challenge_id": 302004, "use_time": 8316},
-        ],
-    },
-}
